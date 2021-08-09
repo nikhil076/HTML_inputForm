1c246d462a346166a88b2e2a9c6ad5ce4267bd95
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-09 23:01:42 +0530

    Added validation user email id in the form

--- js/inputForm.js
+++ js/inputForm.js
@@ -1,11 +1,22 @@
 {
     const name = document.querySelector('#name');
-    const errorName = document.querySelector('.error');
+    const errorName = document.querySelector('#errorName');
     name.addEventListener('input', function () {
         let nameRegex = RegExp('^[A-Z][a-z]{2,}$');
         if (nameRegex.test(name.value))
             errorName.textContent = "";
         else
             errorName.textContent = "Name is Incorrect!";
+    });
+
+
+    const email = document.querySelector('#email');
+    const errorEmail = document.querySelector('#errorEmail');
+    email.addEventListener('input', function () {
+        let emailRegex = RegExp(`^[\\w]+(?:[_+-\\.][\\w]+)*@(?:[\\w]+){1}(?:\\.[a-z]{2,}){1,2}$`);
+        if (emailRegex.test(email.value))
+            errorEmail.textContent = "";
+        else
+            errorEmail.textContent = "Email is Incorrect!";
     });
 }
